9024a231b1031b0b57e8aafa6f7d026930bdaaf1
Joe Conley <josephpconley@gmail.com>
2014-01-13 18:36:37 -0500

    test 12

--- deploy.bat
+++ deploy.bat
@@ -2,11 +2,10 @@ call jekyll build
 call git add .
 call git commit -am %1
 echo one
-call git checkout master
+call git checkout master & call git pull
 echo two
-call git pull
-echo three
 cp -r _site/* . && rm -rf _site/ && touch .nojekyll
+echo three
 call git add .
 call git commit -am %1
 call git push --all origin

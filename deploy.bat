de89122217933c9490d93e6a9b0b8572b0cfe0c6
Joe Conley <josephpconley@gmail.com>
2014-01-13 18:18:21 -0500

    test 5

--- deploy.bat
+++ deploy.bat
@@ -3,6 +3,7 @@ call git commit -am %1
 call jekyll build
 call git checkout master
 call git pull
+rem test
 call cp -r _site/* . && rm -rf _site/ && touch .nojekyll
 call git add .
 call git commit -am %1

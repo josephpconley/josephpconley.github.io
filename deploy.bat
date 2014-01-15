d1d245b61c621dc26acef579ae02584ac81971fc
Joe Conley <josephpconley@gmail.com>
2014-01-15 10:50:36 -0500

    deploy fix 3

--- deploy.bat
+++ deploy.bat
@@ -1,6 +1,6 @@
 call jekyll build
 call git add . && call git commit -am %1
 call git checkout master && call git pull
-cp -r _site/* . && rm -rf _site && touch .nojekyll && echo "Copy done"
+cp -r _site/* . && rm -rf _site/ && touch .nojekyll && echo "Copy done"
 call git add . && call git commit -am %1
 call git push --all origin && git checkout source
